ad77434e3d4475d578e7e4d7f0fc9aeb75ff00fa
Anna Pojawis <annapojawis@gmail.com>
2019-04-07 17:43:58 -0700

    clean up code for review component

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
 const router = require('./routes');
 
 const port = 3004;
 
 const app = express();
 
+app.use(morgan('combined'));
 app.use(cors());
 
 app.use(bodyParser.json());
@@ -14,9 +16,9 @@ app.use(bodyParser.urlencoded({
     extended: true,
 }))
 
-app.use(express.static(__dirname + '../public'));
+app.use('/rooms', express.static(__dirname + '../public'));
 
-app.use('/api', router);
+app.use('/rooms', router);
 
 app.listen(port, function() {
     console.log(`Listening on port: ${port}`);
